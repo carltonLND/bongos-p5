f755c52b64f09df19fcd302d94332ccdce397d01
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-06-20 16:25:23 +0100

    Added type for global VIDEO in p5js

--- global.d.ts
+++ global.d.ts
@@ -2,5 +2,10 @@
 // This file will add both p5 instanced and global intellisence
 import * as p5Global from "p5/global";
 import module from "p5";
+
+declare global {
+  const VIDEO: any;
+}
+
 export = module;
 export as namespace p5;
